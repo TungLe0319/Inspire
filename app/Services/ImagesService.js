1d4f593d78705edd8fde60457732b48d55ac98c9
Tung Le <tung.le0319@gmail.com>
2022-09-19 09:15:09 -0600

    tung le finishing up final touches

--- app/Services/ImagesService.js
+++ app/Services/ImagesService.js
@@ -2,23 +2,15 @@ import { appState } from '../AppState.js';
 import { Image } from '../Models/Image.js';
 import { SandBox } from './AxiosService.js';
 
-
-
 class ImagesService {
   async getImages() {
     const res = await SandBox.get('/api/images', {
       params: {
-        category: `${appState.style.light? 'cat' : 'dog'}`
-
+        category: `${appState.style.light ? 'bird' : 'turtle'}`,
       },
     });
-  
-    
+
     appState.image = new Image(res.data);
-  
   }
-
-
-
 }
 export const imagesService = new ImagesService();
